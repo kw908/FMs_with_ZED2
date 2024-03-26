365921317cfa834af92554a43fd3624286435777
kw908 <derrickwang1996@gmail.com>
2024-03-26 16:38:53 -0400

    moved ekf parameters yaml file to shared path, ekf node publishing

--- kw_robot/launch/kw_slam_launch.py
+++ kw_robot/launch/kw_slam_launch.py
@@ -12,7 +12,8 @@ def generate_launch_description():
        executable='ekf_node',
        name='ekf_filter_node',
        output='screen',
-       parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}])
+       parameters=[os.path.join(pkg_share, 'params/kw_ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}],)
+    
     return LaunchDescription([launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='False',
                                 description='Flag to enable use_sim_time'),
                                 robot_localization_node
